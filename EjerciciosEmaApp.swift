37dd2bc87580a6857831344084fefb6c18042783
EValadezG <117234140+EValadezG@users.noreply.github.com>
2023-06-14 13:25:49 -0600

    Add files via upload

---
+++ EjerciciosEmaApp.swift
@@ -0,0 +1,17 @@
+//
+//  EjerciciosEmaApp.swift
+//  EjerciciosEma
+//
+//  Created by Facultad de Contaduría y Administracíon on 13/06/23.
+//
+
+import SwiftUI
+
+@main
+struct EjerciciosEmaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView(bigTrain: false)
+        }
+    }
+}
